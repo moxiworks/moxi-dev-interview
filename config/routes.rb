7ff5772952c013a70fe19d88a9b0cc3be2944f1a
Dwight VanTuyl <dvantuyl@gmail.com>
2023-03-28 11:27:17 -0700

    Explicit routes w/o 'shallow' syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 Rails.application.routes.draw do
   defaults format: :json do
-    shallow do
-      resources :events do
-        resources :jobs do
-          resources :shifts
-        end
-      end
+    ## Terse `shallow` syntax
+    # shallow do
+    #   resources :events do
+    #     resources :jobs do
+    #       resources :shifts
+    #     end
+    #   end
+    # end
+
+    resources :events, only: [:index, :create, :show, :update, :destroy] do
+      resources :jobs, only: [:index, :create]
     end
 
-    resources :volunteers
+    resources :jobs, only: [:show, :update, :destroy] do
+      resources :shifts, only: [:index, :create]
+    end
+
+    resources :shifts, only: [:show, :update, :destroy]
+
+    resources :volunteers, only: [:index, :create, :show, :update, :destroy]
   end
 end
